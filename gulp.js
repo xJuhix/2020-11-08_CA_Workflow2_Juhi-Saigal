9ef514e24da416404cb4227db9c2cfd9adf979d5
xJuhix <49311688+xJuhix@users.noreply.github.com>
2020-11-08 22:08:59 +0100

    updated gulp.js

--- gulp.js
+++ gulp.js
@@ -2,13 +2,19 @@
 const gulp = require('gulp');
 const { src, dest } = require('gulp');
 const sass = require('gulp-sass');
-const imagemin = require ('gulp-imagemin');
 const minifyCSS = require('gulp-csso');
 const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 
 
-//sass converted to css
+//copying html files from app to dist folder
+gulp.task('html', function() {
+    return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream())
+})
+
+//sass to css
 function css() {
     return src('sass/**/*.scss')
       .pipe(sass())
@@ -17,20 +23,16 @@ function css() {
       .pipe(browserSync.stream())
  }
 
- exports.css = css;
-
- //copying html files from app to dist folder
-gulp.task('html', function() {
-  return gulp.src('app/**/*.html')
-  .pipe(gulp.dest('dist'))
-  .pipe(browserSync.stream())
-})
+exports.css = css;
 
 
-/* img minified */
+// images minified  
 function minifyImg() {
   return gulp  .src("./app/images/*")
-  .pipe(imagemin())
+  .pipe(imagemin({
+    progressive: true,
+    optimizationLevel: 7,
+}))
   .pipe(gulp.dest("./dist/images"));
 }
 
@@ -43,7 +45,7 @@ gulp.task("default",gulp.series("minifyImg","watch"));
 
 
 //browsersync
-function syncWatch(){
+function watch(){
     browserSync.init({
       server: {
         baseDir: ['./app', './dist']
@@ -53,4 +55,4 @@ function syncWatch(){
     gulp.watch('./app/**/*.html').on('change', browserSync.reload)
    }
    
-   exports.watch = syncWatch;
+   exports.watch = watch;
